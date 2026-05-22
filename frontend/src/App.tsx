a9d93382a4ac4d5aa1716e73ca7afa5eb973e5d5
MarcRamisDev <60746268+MarcRamis@users.noreply.github.com>
2026-05-22 16:11:48 +0200

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <main className="app">
+      <h1>RPG Token Maker</h1>
+      <button onClick={() => alert('Hola')}>Hola</button>
+    </main>
+  );
+}
+
+export default App;
